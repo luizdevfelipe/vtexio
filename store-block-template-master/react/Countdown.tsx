722f6856c4abee3fe67b18eb8ea5109b5a4aed3e
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-02-05 13:11:27 -0300

    countdown adaptado à data de lançamento do produto

--- store-block-template-master/react/Countdown.tsx
+++ store-block-template-master/react/Countdown.tsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import { TimeSplit } from './typings/global'
-import { tick, getTwoDaysFromNow } from './utils/time'
+import React, { useState } from 'react'
+import { useQuery } from 'react-apollo'
+import useProduct from 'vtex.product-context/useProduct'
 import { useCssHandles } from 'vtex.css-handles'
 
-interface CountdownProps {
-  targetDate?: string
-}
+import { TimeSplit } from './typings/global'
+import { tick, getTwoDaysFromNow } from './utils/time'
+import productReleaseDate from './graphql/productReleaseDate.graphql'
 
-const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
-const CSS_HANDLES = ['countdown'];
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  targetDate = DEFAULT_TARGET_DATE
-}) => {
+const CSS_HANDLES = ['countdown']
 
+const Countdown: StorefrontFunctionComponent = () => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
     seconds: '00',
   })
 
-  const handles = useCssHandles(CSS_HANDLES);
+  const handles = useCssHandles(CSS_HANDLES)
 
-  tick(targetDate, setTime);
+  const { product } = useProduct()
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: product?.linkText,
+    },
+    ssr: false,
+  })
+
+  if (!product) {
+    return (
+      <div>
+        <span>There is no product context.</span>
+      </div>
+    )
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Error!</span>
+      </div>
+    )
+  }
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
 
   return (
-    <div className={`${handles.countdown} c-muted-1 fw3 tc t-heading-2`}>
-      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    <div className={`${handles.countdown} c-muted-1 db tc`}>
+      <h1>{`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}</h1>
     </div>
-  );
+  )
 }
 
 Countdown.schema = {
@@ -36,9 +65,14 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
+    title: {
+      title: 'I am a title',
+      type: 'string',
+      default: null,
+    },
     targetDate: {
-      title: 'Data final',
-      description: 'Data final utilizada no contador',
+      title: 'Final date',
+      description: 'Final date used in the countdown',
       type: 'string',
       default: null,
     },
