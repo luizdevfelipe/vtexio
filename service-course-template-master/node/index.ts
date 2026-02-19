9a41d7ce07d5934e17b798d3d2f876dc64f350d4
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-02-19 17:08:02 -0300

    configuração para recebimento de eventos

--- service-course-template-master/node/index.ts
+++ service-course-template-master/node/index.ts
@@ -9,6 +9,9 @@ import {
 import { Clients } from './clients'
 import { analytics } from './handlers/analytics'
 
+const THREE_SECONDS_MS = 3 * 1000
+const CONCURRENCY = 10
+
 // Create a LRU memory cache for the Status client.
 // The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
 const memoryCache = new LRUCache<string, any>({ max: 5000 })
@@ -29,6 +32,14 @@ export default new Service<Clients, State, ParamsContext>({
       default: {
         retries: 2,
         timeout: 10000,
+      },
+      events: {
+        exponentialTimeoutCoefficient: 2,
+        exponentialBackoffCoefficient: 2,
+        initialBackoffDelay: 50,
+        retries: 1,
+        timeout: THREE_SECONDS_MS,
+        concurrency: CONCURRENCY,
       },
     },
   },
