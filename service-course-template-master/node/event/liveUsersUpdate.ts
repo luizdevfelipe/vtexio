1891f279191222c750e0aa8b818ae8df4295054d
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-02-22 19:02:07 -0300

    executando o Client de dados a cada execução do evento

--- service-course-template-master/node/event/liveUsersUpdate.ts
+++ service-course-template-master/node/event/liveUsersUpdate.ts
@@ -1,3 +1,8 @@
-export async function updateLiveUsers() {
-    console.log('EVENT: received event');
+import { EventContext } from '@vtex/api'
+import { Clients } from '../clients/index'
+
+export async function updateLiveUsers(ctx: EventContext<Clients>) {
+    const liveUsersProducts = await ctx.clients.analytics.getLiveUsers()
+    console.log('LIVE USERS: ', liveUsersProducts)
+    return true
 }
