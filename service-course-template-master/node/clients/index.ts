9924da599fdf1a1440641eea24272f557e025ffe
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-02-25 09:08:39 -0300

    registro do externalClient criado

--- service-course-template-master/node/clients/index.ts
+++ service-course-template-master/node/clients/index.ts
@@ -1,10 +1,15 @@
 import { IOClients } from '@vtex/api'
 import Analytics from './analytics'
+import GitHubClient from './github'
 
 
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
     public get analytics() {
         return this.getOrSet('analytics', Analytics)
+    }
+
+    public get github() {
+        return this.getOrSet('github', GitHubClient)
     }
 }
