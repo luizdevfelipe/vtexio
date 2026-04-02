98d3059fc4595d28b9f062e462ff303a0daf20e6
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-04-02 15:35:37 -0300

    título carregado via settingsSchema, Props e Tradução
    "Brincando" com as opções de configuração de dados via admin, através das opções de props via Site Editor e dados obtidos por graphql do settingsSchema definido para o app

--- store-block-template-master/react/Title.tsx
+++ store-block-template-master/react/Title.tsx
@@ -1,30 +1,45 @@
-import { FormattedMessage } from "react-intl";
+import React from 'react';
 import { useCssHandles } from "vtex.css-handles";
+import { useQuery } from 'react-apollo'
+import getSettings from './graphql/getSettings.graphql'
+import { FormattedMessage } from 'react-intl'
 
 interface TitleProps {
-    title: string;
+  title: string;
 }
 
 const CSS_HANDLES = ['title'] as const;
 
 const Title: StorefrontFunctionComponent<TitleProps> = ({ title }) => {
-    const handles = useCssHandles(CSS_HANDLES);
-    const titleText = title || <FormattedMessage id="countdown.title" />;
 
-    return (
-        <div className={`${handles.title} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
-            {titleText}
-        </div>
-    )
+  const { data, loading } = useQuery(getSettings, { ssr: false })
+  let resultParsed = null;
+  if (data && !loading) {
+    try {
+      const result = JSON.parse(JSON.stringify(data?.publicSettingsForApp?.message))
+      resultParsed = JSON.parse(result)
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  const handles = useCssHandles(CSS_HANDLES);
+  const titleText = resultParsed?.titleCountdown || title || <FormattedMessage id="countdown.title" />;
+
+  return (
+    <div className={`${handles.title} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
+      {titleText}
+    </div>
+  )
 }
 
 Title.schema = {
-  title: 'editor.countdown-title.title',
-  description: 'editor.countdown-title.description',
+  title: 'countdown.title',
+  description: 'editor.countdown.description',
   type: 'object',
   properties: {
     title: {
-      title: 'editor.countdown.title.title',
+      title: 'editor.countdown.title',
       type: 'string',
       default: null,
     },
