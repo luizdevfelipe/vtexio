20c79dfb4622de60fcb1041a2bfb5e05c77424b8
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-04-06 15:44:56 -0300

    exibindo o resultado de uma query personalizada

--- store-block-template-master/react/adminExample.tsx
+++ store-block-template-master/react/adminExample.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react'
 import { Layout, PageBlock, PageHeader } from 'vtex.styleguide'
+import { useQuery } from 'react-apollo'
+import helloworld from './graphql/helloworld.graphql'
 
 const AdminExample: FC = () => {
+  const { data } = useQuery(helloworld)
   return (
     <Layout fullWidth pageHeader={<PageHeader title="Admin Example" />}>
       <PageBlock>
         <h1>Hello, World!</h1>
+        <p>{data?.helloworld}</p>
       </PageBlock>
     </Layout>
   )
