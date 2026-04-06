3b3117c245cf6d54531706f629c7321d94887f9c
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-04-06 15:51:54 -0300

    mensagem de carregamento para query

--- store-block-template-master/react/adminExample.tsx
+++ store-block-template-master/react/adminExample.tsx
@@ -4,12 +4,12 @@ import { useQuery } from 'react-apollo'
 import helloworld from './graphql/helloworld.graphql'
 
 const AdminExample: FC = () => {
-  const { data } = useQuery(helloworld)
+  const { data, loading } = useQuery(helloworld)
   return (
     <Layout fullWidth pageHeader={<PageHeader title="Admin Example" />}>
       <PageBlock>
         <h1>Hello, World!</h1>
-        <p>{data?.helloworld}</p>
+        <p>{ loading ? 'Loading...' : data?.helloworld}</p>
       </PageBlock>
     </Layout>
   )
