6bc34479063f25c6dc73387f82f596acee2c32b3
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-04-08 15:57:03 -0300

    defineMessages para pré-processamento de traduções

---
+++ store-block-template-master/react/messages/index.ts
@@ -0,0 +1,28 @@
+import { defineMessages } from 'react-intl'
+
+export const messages = defineMessages({
+    editorCountdownTitle: {
+        id: 'admin/editor.countdown.title',
+        defaultMessage: '',
+    },
+    editorCountdownDescription: {
+        id: 'admin/editor.countdown.description',
+        defaultMessage: ' component',
+    },
+    storeCountdownTitle: {
+        id: 'store/countdown.title',
+        defaultMessage: '',
+    },
+    adminExampleNavigationLabel: {
+        id: 'admin/admin-example.navigation.label',
+        defaultMessage: '',
+    },
+    adminExampleNavigationManagement: {
+        id: 'admin/admin-example.navigation.management',
+        defaultMessage: '',
+    },
+    adminExampleNavigationSearchKws: {
+        id: 'admin/admin-example.navigation.search.kws',
+        defaultMessage: '',
+    },
+});
